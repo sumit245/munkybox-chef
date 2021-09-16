471259060b6cdb54fa18d001cbb74719ccc0b0d2
sumit245 <sumitranjan245@gmail.com>
2021-09-16 10:35:34 +0530

    message

--- EndPoints.js
+++ EndPoints.js
@@ -1,5 +1,5 @@
 export const PARTNER_REQUEST =
-  "http://192.168.1.4:5000/api/partnerrequest/";
+  "http://munkybox-admin.herokuapp.com/api/partnerrequest/";
 export const RESTAURANT_URL = "https://munkybox-admin.herokuapp.com/api/newrest";
 export const RESTAURANT_LOGIN =
   "https://munkybox-admin.herokuapp.com/api/newrest/login/";
