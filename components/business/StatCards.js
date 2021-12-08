e1f8ebafff086618b9e177e4810f287fb2ca2e77
sumit245 <sumitranjan245@gmail.com>
2021-12-08 23:12:00 +0530

    main

--- components/business/StatCards.js
+++ components/business/StatCards.js
@@ -1,85 +1,161 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { Badge } from "react-native-paper";
 import { width } from "../../Dimens";
 
 export default function StatCards({ active, cancel, complete }) {
   return (
     <ScrollView horizontal>
       <View>
-        <View style={styles.stat_card}>
-          <Text style={styles.stat_head}>Sales</Text>
-          <Text style={styles.stat_value}>$80</Text>
-          <Text style={styles.stat_label}>1 Order</Text>
+        <View style={[styles.stat_card, { width: width - 6 }]}>
+          <View
+            style={{
+              flexDirection: "row",
+              flex: 1,
+              justifyContent: "space-between",
+            }}
+          >
+            <View>
+              <Text style={styles.stat_head}>Sales</Text>
+            </View>
+            <View>
+              <Badge>2 orders</Badge>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.stat_value}>$200</Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              height: 30,
+              
+            }}
+          >
+            <Text style={styles.stat_label}>Plan</Text>
+            <Text style={styles.stat_label} >Revenue</Text>
+            <Text style={styles.stat_label}>Discount</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              height: 20,
+              borderRadius:1,
+              margin:1,
+              borderWidth:0.2,
+            }}
+          >
+            <Text style={styles.stat_label}>2</Text>
+            <Text style={styles.stat_label}>$150</Text>
+            <Text style={styles.stat_label}>$12</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              height: 20,
+              margin:1,
+              borderRadius:1,
+              borderWidth:0.2,
+            }}
+          >
+            <Text style={styles.stat_label}>15</Text>
+            <Text style={styles.stat_label}>-</Text>
+            <Text style={styles.stat_label}>-</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              height: 20,
+              margin:1,
+              borderRadius:1,
+              borderWidth:0.2,
+            }}
+          >
+            <Text style={styles.stat_label}>30 </Text>
+            <Text style={styles.stat_label}>-</Text>
+            <Text style={styles.stat_label}>-</Text>
+          </View>
         </View>
         {/* Sales */}
+        <View style={{ flexDirection: "row" }}>
+          <View style={[styles.stat_card, { width: width / 3.1 }]}>
+            <Text style={styles.stat_head}>Ads Campaign</Text>
+            <Text style={styles.stat_value}>Due: $0.75 </Text>
+            <Text style={styles.stat_label}>Paid: $0</Text>
+          </View>
+          {/* Ads Campaign */}
+          <View style={[styles.stat_card, { width: width / 3.1 }]}>
+            <Text style={styles.stat_head}>Commission</Text>
+            <Text style={styles.stat_value}>$8</Text>
+          </View>
+          {/* Commission */}
 
-        <View style={styles.stat_card}>
-          <Text style={styles.stat_head}>Adds Campaign</Text>
-          <Text style={styles.stat_value}>Due: $0.75 </Text>
-          <Text style={styles.stat_label}>Paid: $0</Text>
-        </View>
-        {/* Ads Campaign */}
-      </View>
-      <View>
-        <View style={styles.stat_card}>
-          <Text style={styles.stat_head}>Commission Paid</Text>
-          <Text style={styles.stat_value}>$8</Text>
-        </View>
-        {/* Commission */}
+          <View style={[styles.stat_card, { width: width / 3.1 }]}>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text style={styles.stat_head}>Orders</Text>
+              <View>
+                <Badge>2</Badge>
+              </View>
+            </View>
 
-        <View style={styles.stat_card}>
-          <Text style={styles.stat_head}>Orders</Text>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text style={[styles.stat_label, { color: "#f5a617" }]}>
-              In Progress
-            </Text>
-            <Text style={[styles.stat_value, { color: "#f5a617" }]}>
-              {active}
-            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.stat_label}>In Progress</Text>
+              <Text style={[styles.stat_value, { color: "#f5a617" }]}>
+                {active}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.stat_label}>Completed</Text>
+              <Text style={[styles.stat_value, { color: "#22cf6c" }]}>
+                {complete}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.stat_label}>Cancelled</Text>
+              <Text style={[styles.stat_value, { color: "red" }]}>
+                {cancel}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.stat_label}>Rejected</Text>
+              <Text style={[styles.stat_value, { color: "#777" }]}>0</Text>
+            </View>
           </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text style={[styles.stat_label, { color: "#22cf6c" }]}>
-              Completed
-            </Text>
-            <Text style={[styles.stat_value, { color: "#22cf6c" }]}>
-              {complete}
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text style={[styles.stat_label, { color: "red" }]}>Cancelled</Text>
-            <Text style={[styles.stat_value, { color: "red" }]}>{cancel}</Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text style={[styles.stat_label, { color: "red" }]}>Rejected</Text>
-            <Text style={[styles.stat_value, { color: "red" }]}>0</Text>
-          </View>
+          {/* Orders */}
         </View>
-        {/* Orders */}
       </View>
+
       <View>
         <View style={[styles.stat_card, { width: width - 12 }]}>
           <Text style={styles.stat_head}>Menu visits to orders conversion</Text>
@@ -144,6 +220,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 2,
     paddingVertical: 0,
     textAlign: "justify",
+    color: "#000",
   },
   stat_value: {
     fontWeight: "bold",
