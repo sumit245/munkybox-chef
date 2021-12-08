9368692df46931eb0a0b47e6e539d981123d8338
sumit245 <sumitranjan245@gmail.com>
2021-12-08 13:06:25 +0530

    message

--- screens/PastOrders.js
+++ screens/PastOrders.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  View,
-  Text,
   FlatList,
   SafeAreaView,
   StatusBar,
+  StyleSheet,
+  View,
 } from "react-native";
 import Export from "../components/header/Export";
+import Header from "../components/header/Header";
 
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import Item from "../components/pastorders/Item";
+import ToggleLunchDinner from "../components/header/ToggleLunchDinner";
 
 export default function PastOrders({ navigation }) {
   const [orders, setOrders] = useState([]);
+  const [slot, setSlot] = useState("");
   const restaurant = useSelector((state) => state.restaurant);
   const getApiData = async () => {
     const response = await axios.get(
@@ -29,22 +31,41 @@ export default function PastOrders({ navigation }) {
   const renderItem = ({ item }) => (
     <Item item={item} index={item._id} navigation={navigation} />
   );
+  const ListHeaderComponent = () => <Export />;
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
       <Header
         title={restaurant.restaurant_name + ", " + restaurant.restaurant_id}
-      ></Header>
+      >
+        <View style={styles.switch}>
+          <ToggleLunchDinner handleToggle={(e) => setSlot(e)} />
+        </View>
+      </Header>
 
       <FlatList
         data={orders}
         contentContainerStyle={{ paddingBottom: 10 }}
         renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
         keyExtractor={(item) => item._id}
-        ListFooterComponent={<View />}
-        ListFooterComponentStyle={{ height: 10 }}
         showsVerticalScrollIndicator={false}
       />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  mainPage: {
+    flex: 1,
+  },
+  switch: {
+    position: "absolute",
+    right: 4,
+    bottom: 2,
+    color: "#dfdfdf",
+    flexDirection: "row",
+    padding: 4,
+    alignItems: "center",
+  },
+});
