74bd4fcd0f8c53f77179c726e821838980bf3921
sumit245 <sumitranjan245@gmail.com>
2021-12-20 21:50:58 +0530

    Message

--- components/account/Addmeals.js
+++ components/account/Addmeals.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { styles } from "./account.styles";
 
