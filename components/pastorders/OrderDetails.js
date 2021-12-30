e0e0f41ee8ca91f2382343d79b967ffade599c00
sumit245 <sumitranjan245@gmail.com>
2021-12-30 13:07:16 +0530

    Messafre

--- components/pastorders/OrderDetails.js
+++ components/pastorders/OrderDetails.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import Header from "../header/Header";
 import Export from "../header/Export";
@@ -38,96 +39,111 @@ export default function OrderDetails({ route, navigation }) {
       >
         <Icon name="chevron-back" size={24} color="#2277fc" />
       </TouchableOpacity>
-      <View style={styles.formHeader}>
+      <ScrollView
+        contentContainerStyle={{ justifyContent: "flex-start", flex: 1 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <View style={styles.formHeader}>
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>Order id</Text>
+              <Text style={styles.text}>Ordered on</Text>
+              <Text style={styles.text}>Status</Text>
+            </View>
+            <View style={styles.headerRows}>
+              <Text>{order.order_id}</Text>
+              <Text>
+                {moment(order.order_time).format("DD-MMM-YYYY HH:mm a")}
+              </Text>
+              <Text style={{ textTransform: "uppercase" }}>{order.status}</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>User id</Text>
+              <Text style={styles.text}>Phone</Text>
+            </View>
+            <View style={styles.headerRows}>
+              <Text>{order.user_id}</Text>
+              <Text>{order.phone}</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>Email</Text>
+              <Text>{order.email_id}</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>Deliver to</Text>
+              <Text style={{ textTransform: "uppercase", textAlign: "right" }}>
+                {(address_type || "") +
+                  ", " +
+                  (flat_num || "") +
+                  ", " +
+                  (city || "") +
+                  "\n " +
+                  (locality || "") +
+                  ", " +
+                  (postal_code || "")}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>plan</Text>
+              <Text style={styles.text}>Start Date</Text>
+              <Text style={styles.text}>End Date</Text>
+            </View>
+            <View style={styles.headerRows}>
+              <Text>
+                {order.plan === "twoPlan"
+                  ? "2 Days"
+                  : order.plan === "fifteenPlan"
+                  ? "15 Days"
+                  : "30 Days"}
+              </Text>
+              <Text>{order.start_date}</Text>
+              <Text>{order.end_date}</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <View style={styles.headerRows}>
+              <Text style={styles.text}>Price</Text>
+              <Text style={styles.text}>Discount</Text>
+              <Text style={styles.text}>Total</Text>
+            </View>
+            <View style={styles.headerRows}>
+              <Text>{"$" + order.base_price}</Text>
+              <Text>{"$" + order.discount}</Text>
+              <Text>
+                {"$" +
+                  (parseFloat(order.base_price) - parseFloat(order.discount))}
+              </Text>
+            </View>
+          </View>
+        </View>
         <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>Order id</Text>
+          <Text
+            style={{
+              fontStyle: "italic",
+              fontWeight: "bold",
+              color: "#777",
+              marginLeft: 4,
+            }}
+          >
+            Notes: {order.notes}
+          </Text>
+        </View>
+        <View style={styles.table}>
+          <View style={styles.tableHead}>
+            <Text style={styles.text}>Add on</Text>
             <Text style={styles.text}>Ordered on</Text>
-            <Text style={styles.text}>Status</Text>
+            <Text style={styles.text}>PRICE</Text>
           </View>
-          <View style={styles.headerRows}>
-            <Text>{order.order_id}</Text>
-            <Text>
-              {moment(order.order_time).format("DD-MMM-YYYY HH:mm a")}
-            </Text>
-            <Text style={{ textTransform: "uppercase" }}>{order.status}</Text>
-          </View>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>User id</Text>
-            <Text style={styles.text}>Phone</Text>
-          </View>
-          <View style={styles.headerRows}>
-            <Text>{order.user_id}</Text>
-            <Text>{order.phone}</Text>
-          </View>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>Email</Text>
-            <Text>{order.email_id}</Text>
-          </View>
-        </View>
-
-        <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>Deliver to</Text>
-            <Text style={{ textTransform: "uppercase", textAlign: "right" }}>
-              {(address_type || "") +
-                ", " +
-                (flat_num || "") +
-                ", " +
-                (city || "") +
-                "\n " +
-                (locality || "") +
-                ", " +
-                (postal_code || "")}
-            </Text>
-          </View>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>plan</Text>
-            <Text style={styles.text}>Start Date</Text>
-            <Text style={styles.text}>End Date</Text>
-          </View>
-          <View style={styles.headerRows}>
-            <Text>
-              {order.plan === "twoPlan"
-                ? "2 Days"
-                : order.plan === "fifteenPlan"
-                ? "15 Days"
-                : "30 Days"}
-            </Text>
-            <Text>{order.start_date}</Text>
-            <Text>{order.end_date}</Text>
-          </View>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.headerRows}>
-            <Text style={styles.text}>Price</Text>
-            <Text style={styles.text}>Discount</Text>
-            <Text style={styles.text}>Total</Text>
-          </View>
-          <View style={styles.headerRows}>
-            <Text>{"$" + order.base_price}</Text>
-            <Text>{"$" + order.discount}</Text>
-            <Text>
-              {"$" +
-                (parseFloat(order.base_price) - parseFloat(order.discount))}
-            </Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.table}>
-        <View style={styles.tableHead}>
-          <Text style={styles.text}>Add on</Text>
-          <Text style={styles.text}>Ordered on</Text>
-          <Text style={styles.text}>PRICE</Text>
-        </View>
-        {/* {order.add_on.map((data, key) => (
+          {/* {order.add_on.map((data, key) => (
           <View style={styles.tableRow} key={key}>
             <Text style={{ maxWidth: 100, padding: 2 }} numberOfLines={4}>
               {data.items.item}
@@ -138,14 +154,8 @@ export default function OrderDetails({ route, navigation }) {
             <Text style={{ padding: 2 }}>{data.items.price}</Text>
           </View>
         ))} */}
-      </View>
-      <View style={styles.footer}>
-        <Text
-          style={{ fontStyle: "italic", fontWeight: "bold", color: "#777" }}
-        >
-          Notes: {order.notes}
-        </Text>
-      </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
