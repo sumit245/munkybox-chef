e095cf9006f5fd0d46e6591c37017560280bcb6b
sumit245 <sumitranjan245@gmail.com>
2021-12-31 04:39:15 +0530

    Message

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -14,11 +14,14 @@ import {
   SecondaryColor,
   SecondaryDarkColor,
 } from "../Colors";
+import axios from "axios";
 import { avatarify } from "../helpers/truncate_string";
+import Loader from "../helpers/Loader";
 
 const CollapsedContent = ({ item }) => {
   const [isSwitchOn, setIsSwitchOn] = useState(false);
   const [pulled, setPulled] = useState(false);
+  const [loading, setLoading] = useState(false);
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   const { address_type, locality, city, flat_num, postal_code } = item.address;
 
@@ -35,14 +38,38 @@ const CollapsedContent = ({ item }) => {
 
       if (supported) Linking.openURL(link);
     } catch (error) {
-      alert(error)
+      alert(error);
     }
   };
   const theme = {
     ...DefaultTheme,
     fonts: "thin",
   };
+  const getCurrentOrderDetails = async () => {
+    const res = await axios.get(
+      "http://munkybox-admin.herokuapp.com/api/getcurrentorder/getOrderDetails/" +
+        item.order_id
+    );
+    if (res.data !== null) {
+      let { delivered } = res.data;
+      setIsSwitchOn(delivered);
+    }
+  };
 
+  const setCurrentOrderDetails = async () => {
+    setLoading(true);
+    const res = await axios.put(
+      "http://munkybox-admin.herokuapp.com/api/getcurrentorder/getandupdateorderstatus/" +
+        item.order_id,
+      { delivered: true }
+    );
+    if (res.data !== null) {
+      let { delivered } = res.data;
+      console.log(delivered);
+      onToggleSwitch();
+    }
+    setLoading(false);
+  };
   const makeCall = async (number) => {
     let phoneNumber = "";
     if (Platform.OS !== "android") {
@@ -60,182 +87,190 @@ const CollapsedContent = ({ item }) => {
       })
       .catch((err) => console.log(err));
   };
-
-  return (
-    <View style={styles.orderCard}>
-      <View
-        style={{
-          flexDirection: "row",
-          flex: 1,
-          justifyContent: "space-between",
-        }}
-      >
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <View
-            style={{
-              height: 60,
-              width: 60,
-              borderRadius: 60,
-              backgroundColor: "purple",
-              justifyContent: "center",
-              alignItems: "center",
-              marginRight: 8,
-            }}
-          >
-            <Text style={{ fontWeight: "bold", fontSize: 20, color: "#FFF" }}>
-              {avatarify(item.user_name)}
-            </Text>
-          </View>
-          <View>
-            <Text
-              style={[
-                styles.title,
-                { color: "#000", fontSize: 18, marginVertical: 4 },
-              ]}
+  useEffect(() => {
+    getCurrentOrderDetails();
+  }, [item]);
+  if (!loading) {
+    return (
+      <View style={styles.orderCard}>
+        <View
+          style={{
+            flexDirection: "row",
+            flex: 1,
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View
+              style={{
+                height: 60,
+                width: 60,
+                borderRadius: 60,
+                backgroundColor: "purple",
+                justifyContent: "center",
+                alignItems: "center",
+                marginRight: 8,
+              }}
             >
-              {item.order_id}
-            </Text>
-            <Text style={[styles.title, { fontWeight: "bold" }]}>
-              {item.user_name}
-            </Text>
+              <Text style={{ fontWeight: "bold", fontSize: 20, color: "#FFF" }}>
+                {avatarify(item.user_name)}
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={[
+                  styles.title,
+                  { color: "#000", fontSize: 18, marginVertical: 4 },
+                ]}
+              >
+                {item.order_id}
+              </Text>
+              <Text style={[styles.title, { fontWeight: "bold" }]}>
+                {item.user_name}
+              </Text>
+            </View>
           </View>
-        </View>
 
-        <Switch
-          value={isSwitchOn}
-          onValueChange={onToggleSwitch}
-          disabled={isSwitchOn}
-          color={isSwitchOn ? "green" : "red"}
-        />
-      </View>
-
-      <View
-        style={{
-          alignItems: "center",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: 8,
-          paddingTop: 8,
-          borderTopColor: "#ccc",
-          borderTopWidth: 0.2,
-        }}
-      >
-        <TouchableOpacity
-          style={[styles.link, { marginLeft: "20%" }]}
-          onPress={() => openInMap(item.address)}
-        >
-          <Icon name="location-outline" size={24} color={PrimaryLight} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.link}
-          onPress={() => makeCall(item.phone)}
-        >
-          <Icon name="call-sharp" size={24} color="green" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setPulled(!pulled)}>
-          <Icon
-            name={pulled ? "chevron-up-sharp" : "chevron-down-sharp"}
-            size={24}
-            color="#000"
+          <Switch
+            value={isSwitchOn}
+            onValueChange={setCurrentOrderDetails}
+            disabled={isSwitchOn}
+            color={isSwitchOn ? "green" : "red"}
           />
-        </TouchableOpacity>
-      </View>
-
-      {pulled && (
-        <View style={{ marginVertical: 4, }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginVertical: 2,
-            }}
-          >
-            <Text style={{ fontWeight: "bold", fontSize: 16 }}>Notes: </Text>
-            <Text style={{ fontSize: 14 }}>{item.notes || "N/A"}</Text>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginVertical: 2,
-            }}
-          >
-            <Text style={{ fontWeight: "bold", fontSize: 16 }}>
-              Deliver to:{" "}
-            </Text>
-            <Text style={{ fontSize: 14 }}>
-              {flat_num + "," + locality + " " + city + "-" + postal_code ||
-                "N/A"}
-            </Text>
-          </View>
-
-          <View
-            style={{
-              flexDirection: item.add_on.length > 0 ? "column" : "row",
-
-              marginVertical: 2,
-            }}
-          >
-            <Text style={{ fontWeight: "bold", fontSize: 16 }}>Add ons: </Text>
-            {Array.isArray(item.add_on) && item.add_on.length > 0 ? (
-              <View style={{ flexDirection: "column" }}>
-                <View style={styles.row}>
-                  <View style={styles.th}>
-                    <Text
-                      style={{
-                        textAlign: "center",
-                        padding: 4,
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Item
-                    </Text>
-                  </View>
-                  <View style={styles.th}>
-                    <Text
-                      style={{
-                        textAlign: "center",
-                        padding: 4,
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Qty
-                    </Text>
-                  </View>
-                </View>
-                {item.add_on.map((data, key) => (
-                  <View style={styles.row} key={key}>
-                    <View style={styles.th}>
-                      <Text
-                        style={{
-                          textAlign: "center",
-                          padding: 4,
-                        }}
-                      >
-                        {data.item}
-                      </Text>
-                    </View>
-                    <View style={styles.th}>
-                      <Text
-                        style={{
-                          textAlign: "center",
-                          padding: 4,
-                        }}
-                      >
-                        {data.qty}
-                      </Text>
-                    </View>
-                  </View>
-                ))}
-              </View>
-            ) : (
-              <Text style={{ fontSize: 14 }}>{"N/A"}</Text>
-            )}
-          </View>
         </View>
-      )}
-    </View>
-  );
+
+        <View
+          style={{
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 8,
+            paddingTop: 8,
+            borderTopColor: "#ccc",
+            borderTopWidth: 0.2,
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.link, { marginLeft: "20%" }]}
+            onPress={() => openInMap(item.address)}
+          >
+            <Icon name="location-outline" size={24} color={PrimaryLight} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.link}
+            onPress={() => makeCall(item.phone)}
+          >
+            <Icon name="call-sharp" size={24} color="green" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setPulled(!pulled)}>
+            <Icon
+              name={pulled ? "chevron-up-sharp" : "chevron-down-sharp"}
+              size={24}
+              color="#000"
+            />
+          </TouchableOpacity>
+        </View>
+
+        {pulled && (
+          <View style={{ marginVertical: 4 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 2,
+              }}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>Notes: </Text>
+              <Text style={{ fontSize: 14 }}>{item.notes || "N/A"}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 2,
+              }}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                Deliver to:{" "}
+              </Text>
+              <Text style={{ fontSize: 14 }}>
+                {flat_num + "," + locality + " " + city + "-" + postal_code ||
+                  "N/A"}
+              </Text>
+            </View>
+
+            <View
+              style={{
+                flexDirection: item.add_on.length > 0 ? "column" : "row",
+
+                marginVertical: 2,
+              }}
+            >
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                Add ons:{" "}
+              </Text>
+              {Array.isArray(item.add_on) && item.add_on.length > 0 ? (
+                <View style={{ flexDirection: "column" }}>
+                  <View style={styles.row}>
+                    <View style={styles.th}>
+                      <Text
+                        style={{
+                          textAlign: "center",
+                          padding: 4,
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Item
+                      </Text>
+                    </View>
+                    <View style={styles.th}>
+                      <Text
+                        style={{
+                          textAlign: "center",
+                          padding: 4,
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Qty
+                      </Text>
+                    </View>
+                  </View>
+                  {item.add_on.map((data, key) => (
+                    <View style={styles.row} key={key}>
+                      <View style={styles.th}>
+                        <Text
+                          style={{
+                            textAlign: "center",
+                            padding: 4,
+                          }}
+                        >
+                          {data.item}
+                        </Text>
+                      </View>
+                      <View style={styles.th}>
+                        <Text
+                          style={{
+                            textAlign: "center",
+                            padding: 4,
+                          }}
+                        >
+                          {data.qty}
+                        </Text>
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              ) : (
+                <Text style={{ fontSize: 14 }}>{"N/A"}</Text>
+              )}
+            </View>
+          </View>
+        )}
+      </View>
+    );
+  } else {
+    return <Loader />;
+  }
 };
 
 export default function OrderItem({ item, index, meal }) {
