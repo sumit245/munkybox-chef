0e4cd790996d5667a8f0af3188aa1feb9d00e9f5
sumit245 <sumitranjan245@gmail.com>
2022-01-18 10:03:42 -0800

    Messsage

--- components/header/HeaderOrderDispatch.js
+++ components/header/HeaderOrderDispatch.js
@@ -15,6 +15,7 @@ export default function HeaderTabSwitch({
   selected,
   handler,
   setTabHandler,
+  returnCurrentIndex
 }) {
   const [active, setActive] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
