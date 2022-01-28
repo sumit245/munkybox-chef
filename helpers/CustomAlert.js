64ab1cab42bcacb275aa83dbbd99b41a4eee4457
sumit245 <sumitranjan245@gmail.com>
2022-01-29 02:39:37 +0530

    Message

--- helpers/CustomAlert.js
+++ helpers/CustomAlert.js
@@ -26,7 +26,7 @@ const CustomAlert = ({ title, text, cancelHandler, okHandler }) => {
         <Dialog.Content>
           <Paragraph style={{textAlign:"center"}}>{text}</Paragraph>
         </Dialog.Content>
-        <Dialog.Actions>
+        <Dialog.Actions style={{justifyContent:"space-between"}}>
           <Button onPress={cancel}>Cancel</Button>
           <Button onPress={done}>Done</Button>
         </Dialog.Actions>
