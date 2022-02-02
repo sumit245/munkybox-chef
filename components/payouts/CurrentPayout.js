5bcdf0c93e5ce71faa25c075df365a4d37990256
sumit245 <sumitranjan245@gmail.com>
2022-02-01 23:21:53 -0800

    Message

--- components/payouts/CurrentPayout.js
+++ components/payouts/CurrentPayout.js
@@ -12,11 +12,11 @@ export default function CurrentPayout({
     <View style={styles.card}>
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <View>
-          <Text style={styles.bigText}>Current Payout Cycle</Text>
-          <Text style={styles.smallText}>{current_cycle}</Text>
+          <Text style={[styles.smallText,{textTransform:"uppercase",fontSize:14}]}>Current Payout Cycle</Text>
+          <Text style={styles.bigText}>{current_cycle}</Text>
         </View>
         <View>
-          <Text style={styles.bigText}>Payout Date</Text>
+          <Text style={[styles.bigText,{textTransform:"uppercase",fontSize:14}]}>Payout Date</Text>
           <Text style={styles.smallText}>{payout_date}</Text>
         </View>
       </View>
