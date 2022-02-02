9213dc73ccc3de9da1dac99863ab22d6c5b199f0
sumit245 <sumitranjan245@gmail.com>
2022-02-02 13:25:23 +0530

    Message

--- components/payouts/CurrentPayout.js
+++ components/payouts/CurrentPayout.js
@@ -1,4 +1,9 @@
-import { View, Text, TouchableOpacityBase, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacityBase,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { styles } from "../campaign/campaign.styles";
 
@@ -12,22 +17,41 @@ export default function CurrentPayout({
     <View style={styles.card}>
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <View>
-          <Text style={[styles.smallText,{textTransform:"uppercase",fontSize:14}]}>Current Payout Cycle</Text>
+          <Text style={[styles.smallText, { textTransform: "uppercase" }]}>
+            Current Payout Cycle
+          </Text>
           <Text style={styles.bigText}>{current_cycle}</Text>
         </View>
         <View>
-          <Text style={[styles.bigText,{textTransform:"uppercase",fontSize:14}]}>Payout Date</Text>
-          <Text style={styles.smallText}>{payout_date}</Text>
+          <Text style={[styles.smallText, { textTransform: "uppercase" }]}>
+            Payout Date
+          </Text>
+          <Text style={styles.bigText}>{payout_date}</Text>
         </View>
       </View>
-      <View>
+      <View style={{ marginTop: 16 }}>
         <Text style={styles.bigText}>Week So Far</Text>
-        <Text style={styles.bigText}>${revenue}</Text>
+        <Text style={[styles.bigText, { color: "#008000", fontSize: 18 }]}>
+          ${revenue}
+        </Text>
         <Text style={styles.smallText}>{orders} orders</Text>
-          </View>
-          <TouchableOpacity style={{justifyContent:"center"}}>
-              <Text style={[styles.btnText,{textAlign:"center"}]}>View Payout</Text>
-        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={{
+          borderRadius: 6,
+          borderWidth: 0.2,
+          marginHorizontal: 2,
+          padding: 6,
+          height: 44,
+          backgroundColor: "#2962ff",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={[styles.btnText, { textAlign: "center" }]}>
+          View Payout
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
