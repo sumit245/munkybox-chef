d714f5074f465aac3d6c4ec5bfdb904645b44976
sumit245 <sumitranjan245@gmail.com>
2022-03-02 12:06:45 +0530

    message

--- components/header/HeaderwithBack.js
+++ components/header/HeaderwithBack.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import { styles } from "../../styles/headerstyle";
 
 export default function HeaderwithBack(props) {
   return (
-    <View style={styles.header}>
-      <Text style={styles.title}>{props.title}</Text>
+    <View
+      style={[styles.header, { backgroundColor: "#ddd", flexDirection: "row" }]}
+    >
+      <Icon name="chevron-back" size={24} color="#226ccf" />
+      <Text style={[styles.title, { color: "#226ccf", textAlign: "center" }]}>
+        {props.title}
+      </Text>
     </View>
   );
 }
