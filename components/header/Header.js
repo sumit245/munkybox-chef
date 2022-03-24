65d2cca9b53e9a4100e06d5743dfa04838014281
sumit245 <sumitranjan245@gmail.com>
2022-03-25 02:26:20 +0530

    message

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,16 +2,13 @@ import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "../../styles/headerstyle";
 import { showcurrendate } from "../../helpers/commons";
-import { LinearGradient } from "expo-linear-gradient";
 
 export default function Header(props) {
   return (
-    <LinearGradient colors={["#ff9900", "#ff8800"]} style={styles.header}>
-
-      <Text style={[styles.title, { color: "#fff" }]}>{props.title}</Text>
-      <Text style={[styles.subtitle, { color: "#fff" }]}>{showcurrendate()}</Text>
+    <View style={styles.header}>
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.subtitle}>{showcurrendate()}</Text>
       {props.children}
-
-    </LinearGradient>
+    </View>
   );
 }
