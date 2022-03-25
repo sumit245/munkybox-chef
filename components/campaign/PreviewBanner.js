863f2c019fb801ff778141df7dca728fb1b094c8
sumit245 <sumitranjan245@gmail.com>
2022-03-26 01:44:26 +0530

    Messsage

--- components/campaign/PreviewBanner.js
+++ components/campaign/PreviewBanner.js
@@ -9,6 +9,7 @@ import { Checkbox } from "react-native-paper";
 import Loader from "../../helpers/Loader";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { LinearGradient } from "expo-linear-gradient";
 export default function PreviewBanner({ route, navigation }) {
   const {
     title,
@@ -32,7 +33,7 @@ export default function PreviewBanner({ route, navigation }) {
   const submit = async () => {
     const getMyBanner = await axios.get(
       "http://munkybox-admin.herokuapp.com/api/promo/getbannerslength/" +
-        restaurant.restaurant_id
+      restaurant.restaurant_id
     );
     const myBanner = await getMyBanner.data;
     if (myBanner.length !== 0) {
@@ -59,7 +60,7 @@ export default function PreviewBanner({ route, navigation }) {
         promo_code: code,
         discount_type: discount_type,
         discount: discount,
-        status:"active"
+        status: "active"
       };
       const res = await axios.post(
         "http://munkybox-admin.herokuapp.com/api/promo/",
@@ -90,12 +91,12 @@ export default function PreviewBanner({ route, navigation }) {
                 <Text
                   style={[
                     styles.heading,
-                    { textTransform: "uppercase", marginTop: -22,color:"#000" },
+                    { textTransform: "uppercase", marginTop: -22, color: "#000" },
                   ]}
                 >
                   Growth Style
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>Ads</Text>
+                <Text style={[styles.text, { color: "#000" }]}>Ads</Text>
               </View>
             </View>
 
@@ -107,10 +108,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Restaurant
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>
+                <Text style={[styles.text, { color: "#000" }]}>
                   {restaurant.restaurant_name}
                   {"\n"}
                   {restaurant.city +
@@ -130,10 +131,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   CPC AD CAMPAIGN SELECTED
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>
+                <Text style={[styles.text, { color: "#000" }]}>
                   ${rpc}/click ( {title} )
                 </Text>
               </View>
@@ -147,10 +148,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Duration
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>{duration} days </Text>
+                <Text style={[styles.text, { color: "#000" }]}>{duration} days </Text>
               </View>
             </View>
 
@@ -162,10 +163,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Starts On
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>
+                <Text style={[styles.text, { color: "#000" }]}>
                   {start}
                   {"\n"}
                   Ends by {end}
@@ -181,10 +182,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Promo Code
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>{code}</Text>
+                <Text style={[styles.text, { color: "#000" }]}>{code}</Text>
               </View>
             </View>
 
@@ -196,10 +197,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10, marginTop: 4 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Discount
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>${discount}</Text>
+                <Text style={[styles.text, { color: "#000" }]}>${discount}</Text>
               </View>
             </View>
             <View style={[styles.textContainer, { marginBottom: -4 }]}>
@@ -210,10 +211,10 @@ export default function PreviewBanner({ route, navigation }) {
                 style={{ marginLeft: -10, marginTop: 4 }}
               />
               <View>
-                <Text style={[styles.heading, { textTransform: "uppercase",color:"#000" }]}>
+                <Text style={[styles.heading, { textTransform: "uppercase", color: "#000" }]}>
                   Applicable on
                 </Text>
-                <Text style={[styles.text,{color:"#000"}]}>{plan}</Text>
+                <Text style={[styles.text, { color: "#000" }]}>{plan}</Text>
               </View>
             </View>
           </View>
@@ -226,7 +227,7 @@ export default function PreviewBanner({ route, navigation }) {
                 setChecked(!checked);
               }}
             />
-            <Text style={[styles.text,{color:'#000'}]}>
+            <Text style={[styles.text, { color: '#000' }]}>
               I agree with the{" "}
               <Text
                 style={{
@@ -246,22 +247,24 @@ export default function PreviewBanner({ route, navigation }) {
           </View>
         </View>
         <View style={styles.bottomButtonGroup}>
-          <TouchableOpacity
-            style={[
-              styles.actionButton,
-              {
-                width: "100%",
-                backgroundColor: SecondaryDarkColor,
-                borderColor: SecondaryDarkColor,
-              },
-            ]}
-            onPress={submit}
-            disabled={!checked}
-          >
-            <Text style={[styles.btnText, { color: "#FFF" }]}>
-              Activate this campaign
-            </Text>
-          </TouchableOpacity>
+          <LinearGradient colors={["#ff9900", "#ff6600"]} style={[
+            styles.actionButton,
+            {
+              width: "100%",
+              backgroundColor: SecondaryDarkColor,
+              borderColor: SecondaryDarkColor,
+            },
+          ]}>
+            <TouchableOpacity
+
+              onPress={submit}
+              disabled={!checked}
+            >
+              <Text style={[styles.btnText, { color: "#FFF" }]}>
+                Activate this campaign
+              </Text>
+            </TouchableOpacity>
+          </LinearGradient>
         </View>
       </SafeAreaView>
     );
