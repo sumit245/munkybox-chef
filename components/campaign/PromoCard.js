855543f4bdab59851a9e0bf35b69b09e3b2f6cc7
sumit245 <sumitranjan245@gmail.com>
2022-03-26 00:52:23 +0530

    message

--- components/campaign/PromoCard.js
+++ components/campaign/PromoCard.js
@@ -24,88 +24,98 @@ export default function PromoCard({
   cancelHandler,
 }) {
   return (
-    <View style={styles.promocard}>
-      <View
-        style={{ flexDirection: "row", padding: 16, alignItems: "center" }}
-      >
-        {icon === "shopping-sale" ? (
-          <Percentage name="shopping-sale" size={30} color="#ff6600" />
-        ) : (
-          <Icon name={icon} size={30} color="#ff6600" />
-        )}
-        <View style={{ marginLeft: 8, marginBottom: 12, flexShrink: 1 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 16, padding: 2 }}>
-            {head}
-          </Text>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text
-              style={{
-                fontWeight: "bold",
-                color: DARKGRAY,
-                fontSize: 12,
-                marginLeft: 4,
-              }}
-            >
-              {subhead} days
+    <View style={styles.promocontainer}>
+      <Text style={styles.promotitle}>{title}</Text>
+      <View style={styles.promocard}>
+        <View
+          style={{ flexDirection: "row", padding: 16, alignItems: "center" }}
+        >
+          {icon === "shopping-sale" ? (
+            <Percentage name="shopping-sale" size={30} color="#ff6600" />
+          ) : (
+            <Icon name={icon} size={30} color="#ff6600" />
+          )}
+          <View style={{ marginLeft: 8, marginBottom: 12, flexShrink: 1 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 16, padding: 2 }}>
+              {head}
             </Text>
-            {content && (
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
               <Text
                 style={{
                   fontWeight: "bold",
                   color: DARKGRAY,
                   fontSize: 12,
-                  textAlign: "right",
-                  marginLeft: "50%",
+                  marginLeft: 4,
                 }}
               >
-                ${content} /click
+                {subhead} days
               </Text>
-            )}
+              {content && (
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: DARKGRAY,
+                    fontSize: 12,
+                    textAlign: "right",
+                    marginLeft: "50%",
+                  }}
+                >
+                  ${content} /click
+                </Text>
+              )}
+            </View>
           </View>
         </View>
-      </View>
-      <Divider />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        {cancel ? (
-          <TouchableOpacity
-            style={{
-              borderRightWidth: 0.5,
-              borderRightColor: "#777",
-              paddingHorizontal: 10,
-              marginVertical: 4,
-              height: 24,
-            }}
-          >
-            <Text
-              style={[styles.promoaction, { color: "#000" }]}
-              onPress={cancelHandler}
+        <Divider />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {cancel ? (
+            <TouchableOpacity
+              style={{
+                borderRightWidth: 0.5,
+                borderRightColor: "#777",
+                paddingHorizontal: 10,
+                marginVertical: 4,
+                height: 24,
+              }}
             >
-              {cancel}
-            </Text>
-          </TouchableOpacity>
-        ) : (
-          <View />
-        )}
+              <Text
+                style={[styles.promoaction, { color: "#000" }]}
+                onPress={cancelHandler}
+              >
+                {cancel}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <View />
+          )}
 
-        <Text style={styles.promoaction} onPress={okHandler}>
-          {ok}
-        </Text>
+          <Text style={styles.promoaction} onPress={okHandler}>
+            {ok}
+          </Text>
+        </View>
       </View>
     </View>
-
   );
 }
 const styles = StyleSheet.create({
   promocontainer: {
     marginTop: 12,
+  },
+  promotitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FFF",
+    textAlign: "left",
+    marginHorizontal: 4,
+    marginVertical: 10,
   },
   promocard: {
     backgroundColor: WHITE,
