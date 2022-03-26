c0c1358c91a00f97d946dd47c5f23b0fb4fd5bcf
sumit245 <sumitranjan245@gmail.com>
2022-03-27 02:02:39 +0530

    message:

--- components/account/contact.component.js
+++ components/account/contact.component.js
@@ -11,16 +11,18 @@ export default function ContactUs({ navigation }) {
     >
       <View style={{ alignItems: "center", flexDirection: "row" }}>
         <Icon name="mail-sharp" color="#444" size={24} style={{ paddingVertical: 2 }} />
-        <Text style={{ fontSize: 18, color: "#444", margin: 8, paddingVertical: 2, textAlignVertical: "top" }}>
-          Contact Us
-        </Text>
-        <Text
-          style={[
-            styles.label,
-            { fontSize: 12, color: "#777", marginLeft: 34, marginTop: -10 },
-          ]}
-        >   Help & Support
-        </Text>
+        <View>
+          <Text style={{ fontSize: 18, color: "#444", margin: 8, paddingVertical: 2, textAlignVertical: "top" }}>
+            Contact Us
+          </Text>
+          <Text
+            style={[
+              styles.label,
+              { fontSize: 12, color: "#777", marginLeft: 34, marginTop: -10 },
+            ]}
+          >   Help & Support
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
