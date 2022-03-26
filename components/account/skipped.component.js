86f4cb1e46b60db54a8d29652a2354ef80c0728b
sumit245 <sumitranjan245@gmail.com>
2022-03-27 01:57:46 +0530

    message:

--- components/account/skipped.component.js
+++ components/account/skipped.component.js
@@ -12,7 +12,7 @@ export default function Skipped({ navigation }) {
       >
         <View>
           <Text style={{ fontSize: 18, color: "#444", margin: 8,paddingVertical:2 }}>
-            <Icon name="information-circle-outline" color="#444" size={24} />  About us
+            <Icon name="information-circle-outline" color="#444" size={24} style={{paddingVertical:2}} />  About us
           </Text>
         </View>
       </TouchableOpacity>
