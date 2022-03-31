3580884bed9d2cbdafda35d3c73947de84727c5d
sumit245 <sumitranjan245@gmail.com>
2022-03-31 07:09:08 +0530

    message

--- components/account/logout.component.js
+++ components/account/logout.component.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function Logout({ navigation }) {
   const logout = () => {
-    navigation.replace("Login");
+    Alert.alert("Are you sure?", "Are you sure you want to logout", [
+      { text: "No" },
+      { text: "Yes", onPress: navigation.replace("Login") }
+    ])
   };
   return (
     <LinearGradient colors={["#ff9900", "#ff6600"]} style={{
@@ -15,7 +18,7 @@ export default function Logout({ navigation }) {
       justifyContent: 'center',
       alignItems: 'center',
       marginVertical: 12,
-      marginHorizontal:"46%"
+      marginHorizontal: "46%"
     }}>
       <TouchableOpacity
         onPress={logout}
